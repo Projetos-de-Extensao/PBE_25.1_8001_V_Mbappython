c3dd8490b04c69a5ca54326f7caf09587f34ca1b
Arthur <arthurschiller10@gmail.com>
2025-05-13 20:01:28 -0300

    login com backend

--- ilhaDeliveryApp/frontend/App.js
+++ ilhaDeliveryApp/frontend/App.js
@@ -1,22 +1,44 @@
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { View, Text } from 'react-native';
+// import GestaoClientes from './screens/gestaoClientes';
+// import HomeScreen from './screens/HomeScren';
+
+// const Tab = createBottomTabNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator screenOptions={{ headerShown: false }}>
+//         <Tab.Screen name="Início" component={HomeScreen} />
+//         <Tab.Screen name="Produtos" component={GestaoClientes} />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { View, Text } from 'react-native';
-import GestaoClientes from './screens/gestaoClientes';
-import HomeScreen from './screens/HomeScren';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScren'; // sua tela principal
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Início" component={HomeScreen} />
-        <Tab.Screen name="Produtos" component={GestaoClientes} />
+      <Tab.Navigator>
+        <Tab.Screen name="Login" component={LoginScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
+
 
 
 // App.js
