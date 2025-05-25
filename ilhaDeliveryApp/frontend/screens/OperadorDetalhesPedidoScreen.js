e6fc02bc4a51187421898e1310602369a4f3c62a
Arthur <arthurschiller10@gmail.com>
2025-05-25 15:55:29 -0300

    operador login criptografado conserto

--- ilhaDeliveryApp/frontend/screens/OperadorDetalhesPedidoScreen.js
+++ ilhaDeliveryApp/frontend/screens/OperadorDetalhesPedidoScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, Button, Alert, StyleSheet, TextInput } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://192.168.15.3:8000/api';
+const API_URL = 'http://192.168.0.4:8000/api';
 
 export default function OperadorDetalhesPedidoScreen({ route, navigation }) {
   const { pedidoId } = route.params;
