213c30f8db5a072e74ba119301a630163eb838cf
Arthur <arthurschiller10@gmail.com>
2025-05-28 11:35:42 -0300

    ambiente para desenvolvimento faculdade

--- ilhaDeliveryApp/frontend/screens/OperadorLoginScreen.js
+++ ilhaDeliveryApp/frontend/screens/OperadorLoginScreen.js
@@ -8,7 +8,7 @@ export default function OperadorLoginScreen({ navigation }) {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://192.168.0.4:8000/api/operador/token/', {
+      const response = await fetch('http://172.16.6.231:8000/api/operador/token/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ turno, password: senha }),
